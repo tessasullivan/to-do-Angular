e17c8abf1791e955e65fe7fae64e0cd4603464b5
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-06-11 06:02:17 -0700

    Initial build of To Do List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  // firstTask: Task = new Task("Finsish Angular homework and watch video");
+  tasks: Task [] = [
+    new Task('Finish Angular homework'), 
+    new Task('Watch Mosh video')
+  ];
 }
+
